e67bdfa146b28e4148a3e0d264feebc95a49849e
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-03 15:59:57 +0530

    passwords as secure string

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_ssm_parameter" "parameter" {
   type  = "SecureString"
   key_id = "ca96756f-0bb2-4402-b6ae-7383aa4f1c79"
 }
+
+resource "aws_ssm_parameter" "parameter" {
+  count = length(var.passwords)
+  name  = var.passwords[count.index].name
+  value = var.passwords [count.index].value
+  type  = "SecureString"
+  key_id = "ca96756f-0bb2-4402-b6ae-7383aa4f1c79"
+}
