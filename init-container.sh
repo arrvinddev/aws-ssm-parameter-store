6023f1687e6586bd35a04c77b9bdf4031965223d
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-05-15 18:10:33 +0530

    reverting

--- init-container.sh
+++ init-container.sh
@@ -1,4 +1,4 @@
-for params in ${PARAMS}; do 
+for param in ${PARAMS}; do 
   VAR=$(echo $param | awk -F . '{print $NF}' | tr '[:lower]' '[:upper:]')
   VAL=$(aws ssm get-parameters --names $param | jq .Parameters[].Value | sed -e 's/"//g')
   echo $VAR=$VAL
