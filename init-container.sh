f0bbfddc4321c5b7635340dc55bce7366393f7aa
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-05-15 18:10:19 +0530

    params

--- init-container.sh
+++ init-container.sh
@@ -1,4 +1,4 @@
-for param in ${PARAMS}; do 
+for params in ${PARAMS}; do 
   VAR=$(echo $param | awk -F . '{print $NF}' | tr '[:lower]' '[:upper:]')
   VAL=$(aws ssm get-parameters --names $param | jq .Parameters[].Value | sed -e 's/"//g')
   echo $VAR=$VAL
